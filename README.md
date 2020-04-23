756195bd5d2c230ea9647d092db86248488a6c1a
Hanspeter Umilietti <61993276+Umihan@users.noreply.github.com>
2020-04-23 10:30:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Test2_4AEL
+C# Testprogramm für Zahlensysteme
+
+Arbeitet in Gruppen zu max. 4 Personen
+Die Aufgabenpakete sind folgende:
+1.  Anzeige und Eingabeverarbeitung eines Menüs
+2.  Eingabe und Verarbeitung von Zahlen zur Basis 2/8/10/16
+3.  Umrechnung von Zahlen der Basis 8/10/16 in eine Binärzahl
+4.  Umrechnung einer Binärzahl in Zahlen zur Basis 8/10/16
+5.  Ausgabe der Umrechnung
+Jeder Teilnehmer muss mindestens ein Paket bearbeiten.
+
+Completing assignments
+
+You students follow these steps for each assignment:
+
+    To start, fork the repository.
+    Clone the repository to your computer.
+    Modify the files and commit changes to complete your solution.
+    Push/sync the changes up to GitHub.
+    Create a pull request on the original repository to turn in the assignment.
+
---
+++ test4AEL/test4AEL/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//*** Konvertierung in Zahlensysteme 
+namespace test4AEL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Titel("TITEL DER ANWENDUNG", ConsoleColor.Red);
+            Console.WriteLine("ttttt");
+            Menu();
+
+            Console.ReadKey();
+        }
+
+        static void Titel(string titel, ConsoleColor textfarbe)
+        {
+            int breite = Console.WindowWidth;
+            int textlen = titel.Length;
+            ConsoleColor aktTextFarbe = Console.ForegroundColor;    // aktuelle Farbe speichern
+            Console.Clear();
+            Console.ForegroundColor = textfarbe;
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("*");
+            }
+            for (int i = 0; i < (breite-textlen)/2; i++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine(titel);
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("*");
+            }
+            Console.ForegroundColor = aktTextFarbe;
+        }
+        /// <summary>
+        /// Ausgabe eines Auswahlmenüs
+        /// </summary>
+        static void Menu()
+        {
+            Console.WriteLine("\n1) 10 -> 2");
+            Console.WriteLine("2) 10 -> Hex");
+            Console.WriteLine("3) 2  -> 10");
+            Console.WriteLine("\n Bitte einen Menüpunkt auswählen");
+        }
+    }
+}
